3b8ee439225002b2af794f5fcc2c717a637db5a2
Shawn Hassen <Shassen89@gmail.com>
2018-06-08 13:49:53 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,18 +2,24 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne, cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay [1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
 	} else {
-		alert('Sorry, try again');
-	}
+		alert('Sorry, try again.');
+	};
 }
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	console.log('User flipped ' + cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+
+
